7a698165cb4c715d90e866b495008a94c31f411b
fosforizo <163764401+fosforizo@users.noreply.github.com>
2025-05-06 17:47:36 +0300

    Add files via upload

---
+++ src/main/java/gr/uom/myuom/MyuomApplication.java
@@ -0,0 +1,13 @@
+package gr.uom.myuom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyuomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyuomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gr/uom/myuom/MyuomApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.uom.myuom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyuomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
